433dafdd8bb035fb96b648b6bf618d2ccc41f280
Maks <Maksim.Sklianik@gmail.com>
2024-02-05 19:35:36 +0200

    created the project structure. divided the project into components. added styles and dependencies
    (создал структуру проекта. разделил проект на компоненты. добавлены стили и зависимости )

---
+++ blog-app/src/coponents/BlogContent/BlogContent.js
@@ -0,0 +1,50 @@
+import "./BlogContent.css"
+export const BlogContent = () => {
+    return (
+        <>
+        <h1>Sklianik Maksim Blog</h1>
+    <div clas="post">
+        <div className="post1">
+            <h2>POST1</h2>
+            <p>
+                fghfghfgf adfsdgfsfdgfsdgfgdfgdfgdfgdf
+                fgdgfgdfgdfgdfgdfgdfgdfgdfgderterter
+                dfgdfgdfgdfgdfghdgherterteertert
+                dfgrtertertertertertertertertterter
+                dftertertertretertertertertertertert
+                gfdreertertertertertreterterterterte
+
+            </p>
+        </div>
+    </div>
+    <div clas="post2">
+        <div className="post">
+            <h2>POST2</h2>
+            <p>
+                fghfghfgf adfsdgfsfdgfsdgfgdfgdfgdfgdf
+                fgdgfgdfgdfgdfgdfgdfgdfgdfgderterter
+                dfgdfgdfgdfgdfghdgherterteertert
+                dfgrtertertertertertertertertterter
+                dftertertertretertertertertertertert
+                gfdreertertertertertreterterterterte
+
+            </p>
+        </div>
+    </div>
+    <div clas="post">
+        <div className="post3">
+            <h2>POST3</h2>
+            <p>
+                fghfghfgf adfsdgfsfdgfsdgfgdfgdfgdfgdf
+                fgdgfgdfgdfgdfgdfgdfgdfgdfgderterter
+                dfgdfgdfgdfgdfghdgherterteertert
+                dfgrtertertertertertertertertterter
+                dftertertertretertertertertertertert
+                gfdreertertertertertreterterterterte
+
+            </p>
+        </div>
+    </div>
+        </>
+)
+}
---
+++ blog-app/src/coponents/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css'
+
+ export const Header = () =>{
+    return(
+        <header>
+            <nav>
+                <a href="#first"> HOME </a>
+                <a href="#second"> Abount </a>
+                <a href="#third"> Comtact </a>
+            </nav>
+        </header>
+    )
+ }
