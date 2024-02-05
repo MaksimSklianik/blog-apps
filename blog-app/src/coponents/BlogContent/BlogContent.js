74c4f1eab503a3225386449deed780d2c8d8c279
Maks <Maksim.Sklianik@gmail.com>
2024-02-05 21:55:47 +0200

    created the project structure. divided the project into components. added styles and dependencies
    (создал структуру проекта. разделил проект на компоненты. добавлены стили и зависимости )

--- blog-app/src/coponents/BlogContent/BlogContent.js
+++ blog-app/src/coponents/BlogContent/BlogContent.js
@@ -1,50 +1,29 @@
 import "./BlogContent.css"
+import {posts} from "../../shered/projectData";
+import {getAmountOfPosts} from "../../shered/projectlogic";
+
 export const BlogContent = () => {
+
+    const blogPosts = posts.map((item) => {
+        return (
+            <div key={item.id} className="post">
+
+                <h2>{item.title}</h2>
+
+                {item.description}
+            </div>)
+    })
+
     return (
         <>
-        <h1>Sklianik Maksim Blog</h1>
-    <div clas="post">
-        <div className="post1">
-            <h2>POST1</h2>
-            <p>
-                fghfghfgf adfsdgfsfdgfsdgfgdfgdfgdfgdf
-                fgdgfgdfgdfgdfgdfgdfgdfgdfgderterter
-                dfgdfgdfgdfgdfghdgherterteertert
-                dfgrtertertertertertertertertterter
-                dftertertertretertertertertertertert
-                gfdreertertertertertreterterterterte
+            <h1>Sklianik Maksim Blog</h1>
+            <div clas="posts">
+                {blogPosts}
+            </div>
 
-            </p>
-        </div>
-    </div>
-    <div clas="post2">
-        <div className="post">
-            <h2>POST2</h2>
-            <p>
-                fghfghfgf adfsdgfsfdgfsdgfgdfgdfgdfgdf
-                fgdgfgdfgdfgdfgdfgdfgdfgdfgderterter
-                dfgdfgdfgdfgdfghdgherterteertert
-                dfgrtertertertertertertertertterter
-                dftertertertretertertertertertertert
-                gfdreertertertertertreterterterterte
-
-            </p>
-        </div>
-    </div>
-    <div clas="post">
-        <div className="post3">
-            <h2>POST3</h2>
-            <p>
-                fghfghfgf adfsdgfsfdgfsdgfgdfgdfgdfgdf
-                fgdgfgdfgdfgdfgdfgdfgdfgdfgderterter
-                dfgdfgdfgdfgdfghdgherterteertert
-                dfgrtertertertertertertertertterter
-                dftertertertretertertertertertertert
-                gfdreertertertertertreterterterterte
-
-            </p>
-        </div>
-    </div>
+            <div className='count'>
+                <button onClick={() => (getAmountOfPosts(posts))}> Get amout posts</button>
+            </div>
         </>
-)
+    )
 }
